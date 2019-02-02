6e3a8b6107ea8eded75312bb4e07f2e1c21a7c76
Colombini <colombinis@gmail.com>
2019-02-01 23:21:58 -0300

    Agrega imagen y css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
 class Home extends Component  {
   state = {
@@ -24,8 +25,9 @@ class Home extends Component  {
       posts.map(p =>{
         return (
           <div key={p.id} className="post card">
+            <img src={Pokeball} alt="ALt desc"/>
             <div className="card-content">
-              <Link to={'/' + p.id }><span className="card-title">{p.title}</span></Link>
+              <Link to={'/' + p.id }><span className="card-title red-text">{p.title}</span></Link>
               <p>{p.body}</p>
             </div>
           </div>
@@ -36,7 +38,7 @@ class Home extends Component  {
     ) ;
 
     return (
-      <div className="container">
+      <div className="container home">
         <h1 className="center">Home</h1>
         {postList}
       </div>
