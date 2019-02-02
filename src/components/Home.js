b71cc2c665b1cbc45de6c622e4b6b2598ca5da2c
Colombini <colombinis@gmail.com>
2019-02-01 22:37:06 -0300

    Agrega fake posts API desde jsonplaceholder.typicode.com/posts usando axios en componentDidMount

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = (props) =>  {
-  
-  return (
-    <div className="container">
-      <h1 className="center">Home</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa incidunt sint fuga qui quos. Similique sit excepturi at maiores explicabo consectetur esse deleniti fuga eum rerum aut, ipsum cum quia?
-        </p>
-    </div>
-  );
+class Home extends Component  {
+  state = {
+    posts:[]
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      console.log(response);
+      this.setState({
+        posts: response.data.slice(0,10)
+      });
+    })
+
+  }
+
+  render(){
+    const {posts} = this.state;
+
+    const postList = posts.length ? (
+      posts.map(p =>{
+        return (
+          <div key={p.id} className="post card">
+            <div className="card-content">
+              <span className="card-title">{p.title}</span>
+              <p>{p.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No hay posts</div>
+    ) ;
+
+    return (
+      <div className="container">
+        <h1 className="center">Home</h1>
+        {postList}
+      </div>
+    );
+  }
+
+
 }
 
 export default Home;
